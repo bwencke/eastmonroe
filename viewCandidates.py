f88387d8ff07433ab8aec5bb98f3fcba9315bc91
jkoncel <joekoncel@yahoo.com>
2014-02-22 15:58:35 -0500

    fixed buttons

--- viewCandidates.py
+++ viewCandidates.py
@@ -8,11 +8,11 @@ class ViewCandidates(webapp2.RequestHandler):
     def get(self):
         candidates = Candidate.query().fetch(20)
 
-        self.response.out.write('<html><body><table><tr class="head"><td>Name</td><td>&nbsp&nbsp&nbsp</td><td>Email</td></tr>')
+        self.response.out.write('<html><body><form action="/main" method="post"><table><tr class="head"><td>Name</td><td>&nbsp&nbsp&nbsp</td><td>Email</td></tr>')
         for candidate in candidates:
             self.response.out.write('<tr><td>%s</td><td></td><td>%s</td></tr>' %
                                     (candidate.name,
                                     candidate.email))
 
-        self.response.out.write("""<tr><td><button type="button" onClick="window.location='/'">Return</button></td></tr>""")
+        self.response.out.write("""<tr><td><input type="submit" value="Main Menu"></td></tr>""")
         self.response.out.write('</table></body></html>')
