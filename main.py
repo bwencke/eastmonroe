b1d05fd7e77c67c727500985003e1a43ec9d314f
paulgemp <paulgemp@gmail.com>
2014-02-22 10:42:30 -0500

    FUCCCCCCCCCCKERS

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'Hello fuckers!'
 
 @app.route('/joe/')
 def joe():
