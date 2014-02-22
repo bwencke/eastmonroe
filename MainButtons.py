ab61c5c65287956aa0d938295ff7b51fcb3646da
jkoncel <joekoncel@yahoo.com>
2014-02-22 16:05:32 -0500

    fixed Paul's error

--- MainButtons.py
+++ MainButtons.py
@@ -4,9 +4,9 @@ import webapp2
 BUTTONS = """\
 <html>
         <body>
-            <button type="button" onClick="window.location='/viewCandidate'">View Candidates</button>
+            <button type="button" onClick="window.location='/viewCandidates'">View Candidates</button>
             <button type="button" onClick="window.location='/addCandidate'">Add Candidate</button>
-            <button type="button" onClick="window.location='/viewEvent'">View Events</button>
+            <button type="button" onClick="window.location='/viewEvents'">View Events</button>
             <button type="button" onClick="window.location='/addEvent'">Add Event</button>
         </body>
 </html>
